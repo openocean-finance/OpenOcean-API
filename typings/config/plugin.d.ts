2926719c800d7a7306e59377a5caf6e20eedf403
wells <wells>
2022-05-09 18:06:59 +0800

    update

--- typings/config/plugin.d.ts
+++ typings/config/plugin.d.ts
@@ -14,7 +14,6 @@ import 'egg-schedule';
 import 'egg-static';
 import 'egg-jsonp';
 import 'egg-view';
-import 'egg-mysql';
 import 'egg-cors';
 import { EggPluginItem } from 'egg';
 declare module 'egg' {
@@ -31,7 +30,6 @@ declare module 'egg' {
     static?: EggPluginItem;
     jsonp?: EggPluginItem;
     view?: EggPluginItem;
-    mysql?: EggPluginItem;
     cors?: EggPluginItem;
   }
 }
